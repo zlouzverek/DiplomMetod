574f2d25fafae5282e465bb540a2a2f52b867912
zlouzverek <zlouzverek@yandex.ru>
2025-04-07 19:47:14 +0300

    Добавил сервер свой

--- Program.cs
+++ Program.cs
@@ -12,10 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionsString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-   options.UseSqlite("Data Source=Local.db"));
+    options.UseSqlServer(connectionString));
+
+/*builder.Services.AddDbContext<ApplicationDbContext>(options =>
+   options.UseSqlite("Data Source=Local.db"));*/
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
