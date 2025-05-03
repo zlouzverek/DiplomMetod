dc627d689e1af723e3c6603561b2c20fa07c9f18
zlouzverek <zlouzverek@yandex.ru>
2025-05-03 22:16:33 +0300

    InstuctionExport

---
+++ Controllers/InstructionManualController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiplomMetod.Controllers
+{
+    public class InstructionManualController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
