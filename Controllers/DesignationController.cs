0d0606ecb980550c824fba8c8f196620cbd8b120
zlouzverek <zlouzverek@yandex.ru>
2025-05-06 06:41:23 +0300

    View Correct add Designation

---
+++ Controllers/DesignationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiplomMetod.Controllers
+{
+    public class DesignationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
